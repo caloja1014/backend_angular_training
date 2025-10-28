57f7fe0d0bef892a866b6fa8a2c78c047aa1a04e
Carlos Loja <carlos.loja@thebridge.com>
2025-10-28 18:52:03 -0500

    Adding cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index.js';
-
+import cors from 'cors';
 // import { seedDatabase } from "./seed/seedData.js";
 // import { initModels } from './models/index.js';
 const __dirname = path.resolve();
@@ -25,7 +25,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
